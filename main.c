459a388723ea2a8ad83f67bd97059b8bccd6ddaa
ptclll <103357306+ptclll@users.noreply.github.com>
2024-10-30 12:53:20 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "stm32f10x.h"                  // Device header
+#include "OLED.h"
+#include "PWM.h"
+#include "Key.h"
+
+int main(){
+	PWM_Init();
+	//PWM_SetCompare1(50);
+	while(1){
+	
+	
+	}
+}
