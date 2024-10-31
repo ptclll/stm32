94877ec08a4caef119e167400f4bcefd656d8956
ptclll <103357306+ptclll@users.noreply.github.com>
2024-10-31 19:25:11 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 #include "OLED.h"
 #include "PWM.h"
 #include "Key.h"
+#include "Delay.h"
+uint16_t num;
 
 int main(){
 	PWM_Init();
-	//PWM_SetCompare1(50);
-	while(1){
+	OLED_Init();
+	OLED_ShowNum(1,1,67,5);
+	OLED_ShowNum(2,1,num,5);
+	PWM_SetCompare1(50);
+	
+	while(1)
+		{
+		if(num>800){
+			
+			num = 0;			
+			
+			
+		}
+		//ehuf
+		
+		OLED_ShowNum(2,1,num*0.45,5);
 	
 	
 	}
