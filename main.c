516fbc1084dbfa6f9787d9c287fbc5c09cb2037b
ptclll <103357306+ptclll@users.noreply.github.com>
2024-11-01 16:59:41 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,28 @@
 #include "PWM.h"
 #include "Key.h"
 #include "Delay.h"
+#include "LED.h"
 uint16_t num;
 
 int main(){
 	PWM_Init();
 	OLED_Init();
+	OLED_Init();
 	OLED_ShowNum(1,1,67,5);
 	OLED_ShowNum(2,1,num,5);
 	PWM_SetCompare1(50);
-	
+	void LED1_ON(void);
 	while(1)
 		{
-		if(num>800){
+		if(num>400){
 			
 			num = 0;			
-			
-			
+			 PWM_Stop();
+			PWM_SetCompare1(0);
+			Delay_ms(1000);
+			 PWM_Start();
+			PWM_SetCompare1(50);
+			void LED1_Turn(void);
 		}
 		//ehuf
 		
